e8533b3b5383f3d35026d533981eca2d13ecd8e2
Livio Bieri <livio@livio.li>
2015-03-25 23:27:39 +0100

    Minimalistic implementation of Server done.

--- SocketChat/src/ch/fhnw/kvan/chat/socket/client/Client.java
+++ SocketChat/src/ch/fhnw/kvan/chat/socket/client/Client.java
@@ -1,23 +1,36 @@
 package ch.fhnw.kvan.chat.socket.client;
 
+import java.io.IOException;
+
+import ch.fhnw.kvan.chat.gui.*;
+import ch.fhnw.kvan.chat.socket.server.*;
+
 public class Client {
 	
 	static private String host;
 	static private String port;
 	static private String username;
+	static private Server chatRoomServer = new Server();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		if(args.length != 3) {
 			System.err.println("Expecting <host> <port> <username>");
 		}
 		
 		host = args[0];
-		port = args[1];
-		username = args[2]; 
+		port = args[2];
+		username = args[1]; 
 		
 		System.out.println(username);
 		System.out.println(host);
 		System.out.println(port);
+
+		chatRoomServer.connect(host, port);
+		
+		ClientGUI client = new ClientGUI(chatRoomServer, username);
+		
+		client.addParticipant(username);
+		
 	}
 }
