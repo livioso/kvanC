01d936369d82bb9bb7d26812024b4114e4c51906
Livio Bieri <livio@livio.li>
2015-03-27 16:28:46 +0100

    Simple Client - Server communication works now.

--- YetAnotherChat/src/ch/fhnw/kvan/chat/socket/client/Client.java
+++ YetAnotherChat/src/ch/fhnw/kvan/chat/socket/client/Client.java
@@ -1,6 +1,7 @@
 package ch.fhnw.kvan.chat.socket.client;
 
 import ch.fhnw.kvan.chat.general.ChatRoom;
+import ch.fhnw.kvan.chat.interfaces.IChatRoom;
 import ch.fhnw.kvan.chat.utils.In;
 import ch.fhnw.kvan.chat.utils.Out;
 import org.apache.log4j.Level;
@@ -10,7 +11,7 @@ import ch.fhnw.kvan.chat.gui.ClientGUI;
 import java.io.IOException;
 import java.net.Socket;
 
-public class Client {
+public class Client implements IChatRoom {
 
     private static Logger logger = Logger.getLogger(Client.class);
 
@@ -26,6 +27,28 @@ public class Client {
 
     // corresponding gui client instance
     private ClientGUI clientGui;
+
+    /**
+     * The main method; called via refection from RunClient.
+     *
+     * @param args In this order: Hostname Port[4-digits] Username
+     */
+    public static void main(String[] args) {
+
+        try {
+
+            if (args.length != 3) {
+                throw new Exception("Expecting <host> <port> <username> parameters.");
+            }
+
+            // parameter preconditions are checked as well.
+            Client aNewClient = new Client(args[0], args[1], args[2]);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }
+    }
 
     public Client(String username, String hostname, String portnumber) throws Exception {
         this.username = username;
@@ -49,36 +72,51 @@ public class Client {
         logger.info(username + " trying to connect to " + hostname + " on port " + portnumber);
         clientSocket = new Socket(hostname, Integer.parseInt(portnumber));
 
-        //clientInputStream = new In(clientSocket);
-        //clientOutputStream = new Out(clientSocket);
+        clientInputStream = new In(clientSocket);
+        clientOutputStream = new Out(clientSocket);
 
         logger.info("Client connection established.");
     }
 
     private void setupClientUserInterface() {
-        clientGui = new ClientGUI(ChatRoom.getInstance(), username);
+        clientGui = new ClientGUI(this, username);
         clientGui.addParticipant(username);
     }
 
-    /**
-     * The main method; called via refection from RunClient.
-     *
-     * @param args In this order: Hostname Port[4-digits] Username
-     */
-    public static void main(String[] args) {
+    @Override
+    public boolean addParticipant(String name) throws IOException {
+        return false;
+    }
 
-        try {
+    @Override
+    public boolean removeParticipant(String name) throws IOException {
+        return false;
+    }
 
-            if (args.length != 3) {
-                throw new Exception("Expecting <host> <port> <username> parameters.");
-            }
+    @Override
+    public boolean addTopic(String topic) throws IOException {
+        logger.info(username + " is adding topic " + topic);
+        clientOutputStream.println(topic);
+        return true;
+    }
 
-            // parameter preconditions are checked as well.
-            Client aNewClient = new Client(args[0], args[1], args[2]);
+    @Override
+    public boolean removeTopic(String topic) throws IOException {
+        return false;
+    }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error(e.getMessage());
-        }
+    @Override
+    public boolean addMessage(String topic, String message) throws IOException {
+        return false;
+    }
+
+    @Override
+    public String getMessages(String topic) throws IOException {
+        return null;
+    }
+
+    @Override
+    public String refresh(String topic) throws IOException {
+        return null;
     }
 }
--- YetAnotherChat/src/ch/fhnw/kvan/chat/socket/server/ConnectionHandler.java
+++ YetAnotherChat/src/ch/fhnw/kvan/chat/socket/server/ConnectionHandler.java
@@ -1,5 +1,10 @@
 package ch.fhnw.kvan.chat.socket.server;
 
+import ch.fhnw.kvan.chat.utils.In;
+import ch.fhnw.kvan.chat.utils.Out;
+
+import java.net.Socket;
+
 /**
  * This thread is responsible for listing to one
  * particular client for changes.
@@ -8,9 +13,27 @@ package ch.fhnw.kvan.chat.socket.server;
  */
 public class ConnectionHandler implements Runnable {
 
+    Socket clientSocket;
+    In clientInputStream;
+    Out clientOutputStream;
+
+    public ConnectionHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        this.clientOutputStream = new Out(clientSocket);
+        this.clientInputStream = new In(clientSocket);
+
+        Thread me = new Thread(this);
+        me.start();
+    }
 
     @Override
     public void run() {
 
+        System.out.println("Server is listing for user input.");
+
+        while(true) {
+            String inputString = clientInputStream.readLine();
+            System.out.println(inputString);
+        }
     }
 }
