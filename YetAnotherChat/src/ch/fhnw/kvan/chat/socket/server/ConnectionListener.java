464745f1ac25dc38a98205a1a02f63dcf07a67da
Livio Bieri <livio@livio.li>
2015-03-27 12:16:05 +0100

    ConnectionHandler and -Listener added.

---
+++ YetAnotherChat/src/ch/fhnw/kvan/chat/socket/server/ConnectionListener.java
@@ -0,0 +1,12 @@
+package ch.fhnw.kvan.chat.socket.server;
+
+/**
+ * Created by livio on 27/03/15.
+ */
+public class ConnectionListener implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
