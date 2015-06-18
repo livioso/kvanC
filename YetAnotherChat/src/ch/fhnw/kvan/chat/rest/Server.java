9de4f60afa1832726a0a2e2e4feabf8693404ef8
Livio Bieri <livio@livio.li>
2015-06-18 14:53:49 +0200

    Grizzly server setup added

--- YetAnotherChat/src/ch/fhnw/kvan/chat/rest/Server.java
+++ YetAnotherChat/src/ch/fhnw/kvan/chat/rest/Server.java
@@ -1,7 +1,28 @@
 package ch.fhnw.kvan.chat.rest;
 
-/**
- * Created by livio on 16/06/15.
- */
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+
+        final String baseUri = "http://localhost:9998";
+
+        final ResourceConfig rc = ResourceConfig
+                .forApplication(new CustomerApplication());
+
+        System.out.println("Starting Grizzly...");
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(baseUri), rc);
+
+        System.out.println(String.format(
+                "Jersey app started with WADL available at " +
+                        "%sapplication.wadl\nTry out %s\nHit enter to stop it...",
+                baseUri, baseUri));
+
+        System.in.read();
+        httpServer.shutdown();
 }
