f19c859b7ad4d8f97e1e09c9d656c71b45aedcca
Livio Bieri <livio@livio.li>
2015-06-24 10:00:15 +0200

    Successfully first version of this thing

--- YetAnotherChat/src/ch/fhnw/kvan/chat/rest/ChatRoomResource.java
+++ YetAnotherChat/src/ch/fhnw/kvan/chat/rest/ChatRoomResource.java
@@ -2,11 +2,8 @@ package ch.fhnw.kvan.chat.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.CacheControl;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.EntityTag;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.*;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
 @Path("/hello")
@@ -14,18 +11,10 @@ public class ChatRoomResource {
     // private String lastTimestamp = "";
     private String lastTimestamp = String.valueOf(System.currentTimeMillis());
 
-    @Path("/hi")
     @GET
-    public Response getOrder(@Context Request request) {
-        EntityTag tag = new EntityTag(lastTimestamp);
-
-        ResponseBuilder builder = request.evaluatePreconditions(tag);
-        CacheControl cc = new CacheControl();
-        cc.setMaxAge(3000);
-
-        if (builder != null)
-            return builder.cacheControl(cc).build();
-        String order = null;
-        return Response.ok(order).cacheControl(cc).build();
+    @Path("/hi")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
     }
 }
