01ba21c3179329403e88bdd53991a4fcb955f092
Livio Bieri <livio@livio.li>
2015-06-24 10:40:36 +0200

    Implements /users/{username} PUT/DELETE

--- YetAnotherChat/src/ch/fhnw/kvan/chat/rest/ChatRoomResource.java
+++ YetAnotherChat/src/ch/fhnw/kvan/chat/rest/ChatRoomResource.java
@@ -1,20 +1,34 @@
 package ch.fhnw.kvan.chat.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.*;
-import javax.ws.rs.core.Response.ResponseBuilder;
+import ch.fhnw.kvan.chat.general.ChatRoom;
 
-@Path("/hello")
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.io.IOException;
+
+@Path("/chat/chatRoom")
 public class ChatRoomResource {
-    // private String lastTimestamp = "";
-    private String lastTimestamp = String.valueOf(System.currentTimeMillis());
+
+    ChatRoom theChatRoom = ChatRoom.getInstance();
 
     @GET
-    @Path("/hi")
+    @Path("/users")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    public String getUsers() throws IOException {
+        return theChatRoom.getParticipants().toString();
     }
+
+    @PUT
+    @Path("/users/{username}")
+    public void addUser(@PathParam("username") String username) throws IOException {
+        theChatRoom.addParticipant(username);
+    }
+
+    @DELETE
+    @Path("/users/{username}")
+    public void removeUser(@PathParam("username") String username) throws IOException {
+        theChatRoom.removeParticipant(username);
+    }
+
+
 }
